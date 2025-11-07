8896f663cf07d09ad68987e85716dc30c25cc6c1
nandkishorhiwale321 <nandkishoehiwale321@gmail.com>
2025-11-07 18:01:29 +0530

    Access Modifiers

---
+++ Access_1/Impo_2.java
@@ -0,0 +1,10 @@
+package Access_1;
+import Access_mo.Acc_2;
+
+public class Impo_2 {
+	public static void main (String args []) {
+
+	Acc_2 AA = new Acc_2();  //access private veriable ane method of other package in the use of object 
+	AA.show();
+}
+}
