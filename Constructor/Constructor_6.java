75098ce98d8496e36c405d92a8773bd0e2e96f41
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2025-11-09 21:35:53 +0530

    Constructor

---
+++ Constructor/Constructor_6.java
@@ -0,0 +1,24 @@
+package Constructor;
+
+public class Constructor_6 {
+
+	public static void main(String[] args) {
+		System.out.println("hey.....!");
+		
+
+	}
+
+}
+class Ride {
+	
+	public void m1() {
+		
+		m2();
+		
+	}
+	public void m2() {
+		
+		m1();
+	}
+	
+}
