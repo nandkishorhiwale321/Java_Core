e7c98dd75e1adcae821de24b2bcf169fea16ebe6
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2025-11-15 23:05:20 +0530

    even or odd number

---
+++ conditional/Even_odd.java
@@ -0,0 +1,25 @@
+package conditional;
+
+public class Even_odd {
+	public static void main (String []args) {
+		
+		// even or odd number 1 to 50 
+		
+		
+		
+		
+for (int i = 1 ; i <= 50; i++) {
+	
+	if(i % 2 == 0) {
+		System.out.println("Even Number :"+i);
+		
+	}
+	else
+	{
+		System.out.println("odd Number :"+i);
+	}
+}
+	}
+}
+
+
