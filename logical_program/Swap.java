24efd62f614c47138ceee12d5c156292946f7433
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2025-11-29 14:14:40 +0530

    logical program

---
+++ logical_program/Swap.java
@@ -0,0 +1,26 @@
+package logical_program;
+
+public class Swap {
+	public static void main (String [] args) {
+		/*
+		 * Swapping using third variable
+		 */
+		
+		
+		System.out.println("Swap using third veriable :");
+		
+		int A = 10;
+		int B = 5;
+		int c;
+		
+		System.out.println("A :"+A+"\nB : "+B);
+		
+		 c =A;
+		 A =B;
+		 B =c;
+		
+		 System.out.println("A :"+A+"\nB :"+B);
+		
+	}
+
+}
