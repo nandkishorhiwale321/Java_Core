5dfcbdcd41883722578fd2b2259471b274652467
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2025-12-01 14:02:47 +0530

    Even odd number concept

--- java_Basic/Even_or_odd.java
+++ java_Basic/Even_or_odd.java
@@ -5,10 +5,10 @@ public class Even_or_odd {
 	public static void main(String[] args) {
 		int  number = 20;
 		if(number % 2 == 0) {
-			System.out .println(number + "is even number");
+			System.out .println(number + " is even number");
 		}
 		else {
-			System.out.println(number + "is odd number");
+			System.out.println(number + " is odd number");
 		}
 
 	}
