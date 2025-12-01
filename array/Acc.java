5ca9bd7901eee8868e1511a732e988e4a60819b6
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2025-12-01 13:59:24 +0530

    Concepet...!

--- array/Acc.java
+++ array/Acc.java
@@ -1,10 +1,9 @@
 package array;
-import oops.Access_1;
+
 public class Acc {
 
 	public static void main (String args[]) {
 		
-		Access_1 A = new Access_1();
-		A.show();
+		System.out.println("Array");
 	}
 }
